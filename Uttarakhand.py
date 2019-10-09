f34a2e639f0deaea504945cdc4be00ad46577b8d
salman98ansari <salman98101@gmail.com>
2019-10-09 22:54:21 +0530

    done

---
+++ Uttarakhand.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  9 18:00:58 2019
+
+@author: salman
+"""
+
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import requests
+url= requests.get('https://indianhelpline.com/UTTARAKHAND/').text
+
+soup = BeautifulSoup(url, 'lxml')
+table= soup.findAll('tr')
+#print(table[42])
+file =open('Uttarakhand.csv','w')
+header=' Name , Number\n'
+file.write(header)
+for td in table[42: ]:
+    try:
+        tr = td.findAll('td')
+        name = tr[0].text
+        number = tr[1].text
+        print(name, number)
+    except:
+        pass 
+    file.write(name.replace(',','') + ',' + number.replace(',','|')+ '\n')
+file.close()
+
